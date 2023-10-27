8c83ef25eb890c1c02bb28d8690336e80fce3eda
AnushaGurijala1207 <136043406+AnushaGurijala1207@users.noreply.github.com>
2023-10-27 00:42:33 -0500

    added resoruce class i.e instruments at the local repo

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var instrumentsRouter = require('./routes/instruments');
 var app = express();
 
 // view engine setup
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/instruments', instrumentsRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
